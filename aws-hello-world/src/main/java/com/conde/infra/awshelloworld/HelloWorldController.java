a090514ba2340b7dafcc68aade4a1acfe7173eec
Rajeev <rajeev.chotai@gmail.com>
2020-07-18 13:06:43 +0530

    java code

---
+++ aws-hello-world/src/main/java/com/conde/infra/awshelloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.conde.infra.awshelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("/")
+  public String helloWorld(){
+    return "Hello World";
+  }
+
+}
---
+++ aws-hello-world/src/test/java/com/conde/infra/awshelloworld/AwsHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conde.infra.awshelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
