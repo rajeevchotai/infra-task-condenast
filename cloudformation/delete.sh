d3f9e95ced8bc46c52bb5f49bf5f060a74cea5a2
Rajeev <rajeev.chotai@gmail.com>
2020-07-18 23:17:18 +0530

    updated to add delele for fargate cloudformation

--- cloudformation/delete.sh
+++ cloudformation/delete.sh
@@ -1,5 +1,9 @@
 # Turning off the AWS pager so that the CLI doesn't open an editor for each command result
 export AWS_PAGER=""
 
+
 aws cloudformation delete-stack --stack-name hello-world-code-pipeline
 aws cloudformation wait stack-delete-complete --stack-name hello-world-code-pipeline
+
+aws cloudformation delete-stack --stack-name dev-FargateECSCluster
+aws cloudformation wait stack-delete-complete --stack-name dev-FargateECSCluster
