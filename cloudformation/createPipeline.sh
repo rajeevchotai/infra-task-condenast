8e94ad81196a9a93803377b3fc1fd4eb6ebdcf85
Rajeev <rajeev.chotai@gmail.com>
2020-07-19 16:18:33 +0530

    removed oauth token from sh

--- cloudformation/createPipeline.sh
+++ cloudformation/createPipeline.sh
@@ -4,7 +4,7 @@ aws cloudformation create-stack \
   --stack-name hello-world-code-pipeline \
   --template-body file://CodePipeline.yml  \
   --parameters \
-      ParameterKey=GithubOAuthToken,ParameterValue=2216f2c7e5ea89a55320be07e3eaefa704fc398b \
+      ParameterKey=GithubOAuthToken,ParameterValue=<oauth-token> \
   --capabilities CAPABILITY_IAM --profile infra-task-condenast
 
   aws cloudformation wait stack-create-complete --stack-name hello-world-code-pipeline --profile infra-task-condenast
